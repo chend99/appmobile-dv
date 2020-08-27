b0894941f73331d09bc1de0d1b91fe509e787b2d
Chen David <59599900+chend99@users.noreply.github.com>
2020-08-27 20:56:52 -0300

    Update ejercicio7.js

--- guia-js/2)metodos-arrays/ejercicio7.js
+++ guia-js/2)metodos-arrays/ejercicio7.js
@@ -1,4 +1,4 @@
-/*6- 7- Define la función hayAlgunNegativo, que dada una lista de números nos dice si hay algún negativo*/
+/*7- Define la función hayAlgunNegativo, que dada una lista de números nos dice si hay algún negativo*/
 function hayAlgunNegativo(array){
     return array.some(elem => elem<0);
 }
